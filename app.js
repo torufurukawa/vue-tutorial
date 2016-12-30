0027be270a9338b2ae72f25a94c54c5a2282c2dd
Toru Furukawa <torufurukawa@gmail.com>
2016-12-30 16:19:22 +0900

    Add computed property

--- app.js
+++ app.js
@@ -64,3 +64,15 @@ var app7 = new Vue({
     ]
   }
 });
+
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: "Hello"
+  },
+  computed: {
+    reversedMessage: function () {
+      return this.message.split('').reverse().join('');
+    }
+  }
+});
