76a8e96e97e2438c7cd840005ee6d0083aeecb83
Toru Furukawa <torufurukawa@gmail.com>
2016-12-30 15:21:20 +0900

    Add components

--- app.js
+++ app.js
@@ -48,3 +48,19 @@ var app6 = new Vue({
     message: 'Helloo Vue'
   }
 });
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { text: 'Vegitables' },
+      { text: 'Cheese' },
+      { text: 'Whatever else humans are supposed to eat'}
+    ]
+  }
+});
